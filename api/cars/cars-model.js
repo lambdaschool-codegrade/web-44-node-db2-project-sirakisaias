248c84173458b407f5d51d30f4ef254ac81a80e0
sirakisaias <sirakisaias@gmail.com>
2021-08-11 00:51:20 -0700

    finished cars router and model for get only

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,19 @@
+const db = require('../../data/db-config')
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db('cars').where('id', id).first()
 }
 
-const create = () => {
+async function create ({vin, make, model, mileage, title, transmission}) {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert({ vin, make, model,  mileage, title, transmission }) // [67] the id of the new rec
+  const newCars = await getById(id)
+  return newCars
 }
+
+module.exports = {getAll, getById, create}
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
 // DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+
+router.get('/api/cars', (res, req) => {
+    Cars.getAll()
+    .then(cars =>{
+        res.json(cars)
+    })
+    .catch(err =>{
+        res.status(500).json({message: err.message})
+    })
+})
+
+module.exports = router
